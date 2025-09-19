a5517e6f77c421414e4dc4c6adf085086d5dda4e
lucadileo9 <lucadileo70@gmail.com>
2025-09-19 07:12:06 -0400

    fix: lint problems

--- service/routes.py
+++ service/routes.py
@@ -65,6 +65,8 @@ def create_accounts():
     ######################################################################
     # LIST ALL ACCOUNTS
     ######################################################################
+
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -84,6 +86,8 @@ def list_accounts():
 ######################################################################
 
 # ... place you code here to READ an account ...
+
+
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     """
@@ -96,16 +100,16 @@ def get_accounts(account_id):
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
 
-
-
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
 # ... place you code here to UPDATE an account ...
-    ######################################################################
-    # UPDATE AN EXISTING ACCOUNT
-    ######################################################################
+######################################################################
+# UPDATE AN EXISTING ACCOUNT
+######################################################################
+
+
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
@@ -128,7 +132,24 @@ def update_accounts(account_id):
 ######################################################################
 
 # ... place you code here to DELETE an account ...
+    ######################################################################
+    # DELETE AN ACCOUNT
+    ######################################################################
 
+
+@app.route("/accounts/<int:account_id>", methods=["DELETE"])
+def delete_accounts(account_id):
+    """
+    Delete an Account
+    This endpoint will delete an Account based on the account_id that is requested
+    """
+    app.logger.info("Request to delete an Account with id: %s", account_id)
+
+    account = Account.find(account_id)
+    if account:
+        account.delete()
+
+    return "", status.HTTP_204_NO_CONTENT
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
